00f0162c72cdca0da11ced1ffd20f2152af2eaa9
Asaf Manela <AsafManela@users.noreply.github.com>
2019-06-28 08:54:37 -0500

    replaced distributed local cluster with multi-threading

--- test/helpers.jl
+++ test/helpers.jl
@@ -1,20 +1,6 @@
 @testset "helpers" begin
 
-# test convert methods for SharedArray
 Random.seed!(1)
-A = sprand(100,4,0.3)
-sA = convert(SharedArray,A)
-@test A == sA
-
-a = view(A,:,2:3)
-@test a == getindex(A,:,2:3)
-@test a == getindex(sA,:,2:3)
-
-sa = convert(SharedArray,a)
-@test a == sa
-
-b = view(sA,:,2:3)
-@test a == b
 
 # test counts matrix coverter to float64 entries
 fpcounts = HurdleDMR.fpcounts
